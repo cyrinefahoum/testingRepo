0989c8cd06fe868dc05528e116af54828cad5ee4
AhmadTawil1 <ahmadtawil.se@outlook.com>
2024-11-18 22:19:54 +0200

    try new branch

--- Hello/src/main/Main.java
+++ Hello/src/main/Main.java
@@ -4,7 +4,7 @@ public class Main {
 
 	public static void main(String[] args) {
 		System.out.println("Hello world");
-		System.out.println("Hello world1");
+		System.out.println("newBranch1");
 
 	}
 
